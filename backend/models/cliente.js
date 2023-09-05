407c5bfd620158c71b658501adeca90a8537227f
Rafael <rafa.developers@gmmail.com>
2023-09-05 00:19:14 -0500

    Registro de un Cliente

---
+++ backend/models/cliente.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let ClienteSchema = Schema({
+    nombres: { type: String, required: true},
+    apellidos: { type: String, required: true},
+    pais: { type: String, required: false},
+    email: { type: String, required: true},
+    password: { type: String, required: true},
+    perfil: { type: String, default: 'perfil.png', required: true},
+    telefono: { type: String, required: false},
+    genero: { type: String, required: false},
+    f_nac: { type: String, required: false},
+    cedula: { type: String, required: false},
+});
+
+module.exports = mongoose.model('cliente', ClienteSchema);
