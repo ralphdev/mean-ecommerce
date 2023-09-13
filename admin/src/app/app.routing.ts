be176541f66b45d7d147c0d0d3c2e68c58fe7c9e
Rafael <rafa.developers@gmmail.com>
2023-09-13 04:48:15 -0500

    Login y Service

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -1,10 +1,20 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 import { InicioComponent } from "./components/inicio/inicio.component";
+import { LoginComponent } from "./components/login/login.component";
+import { adminGuard } from "./guards/admin.guard";
 
 
 const appRoute: Routes = [
-    { path: '', component: InicioComponent }
+    { 
+        path: '', 
+        component: InicioComponent, 
+        canActivate: [adminGuard] 
+    },
+    { 
+        path: 'login', 
+        component: LoginComponent 
+    }
 ];
 
 export const appRoutingProviders: any[] = [];
