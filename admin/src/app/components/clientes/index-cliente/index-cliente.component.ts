40f68d5392b7292aef81455a61c698225bec00b6
ralphdev <rafa.developers@gmail.com>
2023-10-03 11:38:47 -0500

    Componente Listar Clientes

--- admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
+++ admin/src/app/components/clientes/index-cliente/index-cliente.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-index-cliente',
   templateUrl: './index-cliente.component.html',
   styleUrls: ['./index-cliente.component.css']
 })
+
 export class IndexClienteComponent {
+
+  constructor(
+    private _clienteService: ClienteService
+  ) {
+
+  }
+
+  public clientes: any[] = [];
+  public filtro_apellidos: string = '';
+  public filtro_correo: string = '';
+
+  // * Variables de paginación
+  public page: number = 1;
+  public pageSize: number = 10;
+  public totalItems: any;
+  public previousPage: any;
+
+  public token: any;
+  public load_data: boolean = true;
+
+  ngOnInit(): void {
+    this.initData();
+  }
+
+  initData() {
+
+    this._clienteService.listClientesFiltroAdmin(null, null).subscribe({
+      next: response => {
+
+        this.clientes = response.data;
+        this.load_data = false;
+      },
+      error: error => {
+
+        console.error(error);
+      }
+    })
+  }
+
+  loadPage(page: number) {
+    if (page !== this.previousPage) {
+      this.previousPage = page;
+      this.initData();
+    }
+  }
+
+
+
+  filtro(tipo: string) {
+
+  }
+
+  eliminar(id: string) {
+
+  }
 
 }
