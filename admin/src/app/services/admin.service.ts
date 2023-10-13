b69d52e690222cfb462c99433b4e0b7cfebf33c5
ralphdev <rafa.developers@gmail.com>
2023-10-13 00:19:38 -0500

    Fix Login admin

--- admin/src/app/services/admin.service.ts
+++ admin/src/app/services/admin.service.ts
@@ -29,32 +29,59 @@ export class AdminService {
     return localStorage.getItem('token');
   }
 
-  isAuthenticated(allowRoles: string[]): boolean {
+  public isAuthenticated(allowRoles: string[]): boolean {
 
     const token:any = localStorage.getItem('token');
 
     const helper = new JwtHelperService();
     const decodedToken = helper.decodeToken(token);
 
-    console.log(decodedToken, 'isAuthenticated');
-
     try {
 
-
       if(!token){
+
         return false;
       }
 
       if(!decodedToken){
+
         localStorage.removeItem('token');
         return false;
       }
 
     } catch (error) {
+
       localStorage.removeItem('token');
       return false;
     }
 
     return allowRoles.includes(decodedToken['role']);
+  }
+
+  actualizaConfigAdmin(id:any, data:any, token:any): Observable<any>{
+
+    if(data.logo){
+
+      let headers = new HttpHeaders({ 'Authorization':token });
+
+      const fd = new FormData();
+      fd.append('titulo', data.titulo);
+      fd.append('serie', data.serie);
+      fd.append('correlativo', data.correlativo);
+      fd.append('categorias', JSON.stringify(data.categorias));
+      fd.append('logo', data.logo);
+
+      return this._http.put(this.url+'actualiza-config-admin/'+id, fd, { headers:headers });
+
+    }else{
+
+      let headers = new HttpHeaders({ 'Content-Type':'application/json', 'Authorization':token });
+      return this._http.put(this.url+'actualiza-config-admin/'+id,data,{ headers:headers });
+    }
+  }
+
+  obtenerConfigAdmin(token:any): Observable<any>{
+    let headers = new HttpHeaders({'Content-Type':'application/json', 'Authorization':token });
+    return this._http.get(this.url+'obtener-config-admin',{ headers:headers });
   }
 }
