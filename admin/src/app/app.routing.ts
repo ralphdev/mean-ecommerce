888dee8b39356dfe4f01da5a4c507f6242823b5f
ralphdev <rafa.developers@gmail.com>
2023-10-14 14:10:46 -0500

    Componente: Galeria, Reviews, Variedad

--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -8,9 +8,14 @@ import { IndexClienteComponent } from "./components/clientes/index-cliente/index
 import { CreateClienteComponent } from "./components/clientes/create-cliente/create-cliente.component";
 import { EditClienteComponent } from "./components/clientes/edit-cliente/edit-cliente.component";
 
+import { ConfigComponent } from "./components/config/config.component";
 import { IndexProductoComponent } from "./components/productos/index-producto/index-producto.component";
 import { CreateProductoComponent } from "./components/productos/create-producto/create-producto.component";
-import { ConfigComponent } from "./components/config/config.component";
+import { UpdateProductoComponent } from "./components/productos/update-producto/update-producto.component";
+import { VariedadProductoComponent } from "./components/productos/variedad-producto/variedad-producto.component";
+import { GaleriaProductoComponent } from "./components/productos/galeria-producto/galeria-producto.component";
+import { ReviewsProductoComponent } from "./components/productos/reviews-producto/reviews-producto.component";
+import { InventarioProductoComponent } from "./components/productos/inventario-producto/inventario-producto.component";
 
 const appRoute: Routes = [
   { path: '', component: InicioComponent, canActivate: [adminGuard] },
@@ -26,6 +31,11 @@ const appRoute: Routes = [
       //** Productos Rutas */
       { path: 'productos', component: IndexProductoComponent, canActivate: [adminGuard] },
       { path: 'productos/registro', component: CreateProductoComponent, canActivate: [adminGuard] },
+      { path: 'productos/:id', component: UpdateProductoComponent, canActivate: [adminGuard] },
+      { path: 'productos/galeria/:id', component: GaleriaProductoComponent, canActivate: [adminGuard] },
+      { path: 'productos/inventario/:id', component: InventarioProductoComponent, canActivate: [adminGuard] },
+      { path: 'productos/variedades/:id', component: VariedadProductoComponent, canActivate: [adminGuard] },
+      { path: 'productos/reviews/:id', component: ReviewsProductoComponent, canActivate: [adminGuard] },
 
       //** Configuraciones Admin */
       { path: 'configuraciones', component: ConfigComponent, canActivate: [adminGuard] },
---
+++ admin/src/app/components/productos/galeria-producto/galeria-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria-producto',
+  templateUrl: './galeria-producto.component.html',
+  styles: [
+  ]
+})
+export class GaleriaProductoComponent {
+
+}
