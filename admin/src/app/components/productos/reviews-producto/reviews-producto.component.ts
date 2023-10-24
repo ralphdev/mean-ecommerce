2e3b33d1a284e25e300b91d2f3472e6de8991fbf
ralphdev <rafa.developers@gmail.com>
2023-10-23 20:39:49 -0500

    Reviews Component Admin

--- admin/src/app/components/productos/reviews-producto/reviews-producto.component.ts
+++ admin/src/app/components/productos/reviews-producto/reviews-producto.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { global } from 'src/app/services/global';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-reviews-producto',
@@ -6,6 +9,63 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ReviewsProductoComponent {
+export class ReviewsProductoComponent implements OnInit{
+
+    public id:any;
+    public token:any;
+    public _iduser:any;
+    public producto:any = {};
+    public reviews: Array<any> = [];
+    public url:any;
+    public loadBtn = false;
+
+  public page:number = 1;
+  public pageSize:number = 15;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _productoService: ProductoService
+  ) {
+
+    this.token = localStorage.getItem('token');
+    this._iduser = localStorage.getItem('_id');
+    this.url = global.url;
+  }
+
+  ngOnInit(): void {
+
+    this._route.params.subscribe( params => {
+
+      this.id = params['id'];
+
+      this._productoService.obtenerProductoAdmin(this.id, this.token).subscribe({
+
+        next: response => {
+
+          if(response.data == undefined){
+
+            this.producto = undefined;
+          }else{
+
+            this.producto = response.data;
+
+            this._productoService.obtenerReviewsPublico(this.producto._id).subscribe({
+
+              next: response => {
+                this.reviews = response.data;
+              },
+              error: error => {
+                console.log(error);
+              }
+            });
+          }
+        },
+        error: error=>{
+
+          console.log(error);
+        }
+      });
+    });
+  }
 
 }
